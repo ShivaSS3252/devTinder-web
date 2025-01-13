f38ffdac766bf268c6fccdd5c93fddc4911fe2d6
= <=>
2025-01-13 13:31:03 +0530

    build the usercard

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,33 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useEffect } from "react";
+import { Base_url } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 const Feed = () => {
-  return <div>Feed</div>;
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => store.feed);
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(Base_url + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed?.data[2]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
