3ab1135f76cd1f8b89fb7d3fa03c25ffb20b9f8b
= <=>
2025-01-13 22:08:01 +0530

    requests page

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -25,13 +25,16 @@ const Connections = () => {
   if (!connections) return;
   if (connections.length === 0) return <h1>No connections found</h1>;
   return (
-    <div className=" text-center my-20">
+    <div className="text-center my-20">
       <h1 className="text-bold text-white text-3xl">Connections</h1>
       {connections.map((connection) => {
-        const { firstName, lastName, photoUrl, age, gender, about } =
+        const { _id, firstName, lastName, photoUrl, age, gender, about } =
           connection;
         return (
-          <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+          <div
+            key={_id}
+            className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+          >
             <div>
               <img
                 alt="photo"
