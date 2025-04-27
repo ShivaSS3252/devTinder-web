7210a03dc95a79d7857dba4c3d772b501cb72d13
= <=>
2025-04-28 00:02:33 +0530

    err status changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,6 @@ import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { clearFeed } from "../utils/feedSlice";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
@@ -60,114 +58,110 @@ const Login = () => {
   };
 
   return (
-    <>
-      <Navbar />
-      <div className="flex justify-center my-10 mb-20">
-        <div className="card bg-base-300 w-96 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title justify-center text-xl font-bold uppercase tracking-wide">
-              {isLoginForm ? "Login" : "Sign Up"}
-            </h2>
-            <div>
-              {!isLoginForm && (
-                <>
-                  <label className="form-control w-full max-w-xs my-2">
-                    <div className="label">
-                      <span className="label-text font-medium">First Name</span>
-                    </div>
-                    <input
-                      type="text"
-                      value={firstName}
-                      className="input input-bordered w-full max-w-xs"
-                      onChange={(e) => setFirstName(e.target.value)}
-                    />
-                  </label>
-                  <label className="form-control w-full max-w-xs my-2">
-                    <div className="label">
-                      <span className="label-text font-medium">Last Name</span>
-                    </div>
-                    <input
-                      type="text"
-                      value={lastName}
-                      className="input input-bordered w-full max-w-xs"
-                      onChange={(e) => setLastName(e.target.value)}
-                    />
-                  </label>
-                </>
-              )}
-              <label className="form-control w-full max-w-xs my-2">
-                <div className="label">
-                  <span className="label-text font-medium">Email ID</span>
-                </div>
-                <input
-                  type="text"
-                  value={emailId}
-                  className="input input-bordered w-full max-w-xs"
-                  onChange={(e) => setEmailId(e.target.value)}
-                />
-              </label>
-              <label className="form-control w-full max-w-xs my-2">
-                <div className="label">
-                  <span className="label-text font-medium">Password</span>
-                </div>
-                <div className="relative">
+    <div className="flex justify-center my-10 mb-20">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center text-xl font-bold uppercase tracking-wide">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
+          <div>
+            {!isLoginForm && (
+              <>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text font-medium">First Name</span>
+                  </div>
                   <input
-                    type={showPassword ? "text" : "password"}
-                    value={password}
+                    type="text"
+                    value={firstName}
                     className="input input-bordered w-full max-w-xs"
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
-                  <span
-                    className="absolute right-3 top-3 cursor-pointer text-gray-500 hover:text-gray-700"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                  >
-                    {showPassword ? (
-                      <AiOutlineEyeInvisible size={20} />
-                    ) : (
-                      <AiOutlineEye size={20} />
-                    )}
-                  </span>
-                </div>
-              </label>
-            </div>
-            <p className="text-red-500">{error}</p>
-            <div className="card-actions justify-center m-2">
-              <button
-                className="btn btn-primary uppercase"
-                onClick={isLoginForm ? handleLogin : handleSignUp}
-              >
-                {isLoginForm ? "Login" : "Sign Up"}
-              </button>
-            </div>
-
-            <p
-              className="m-auto cursor-pointer py-2 font-semibold"
-              onClick={() => {
-                setIsLoginForm((value) => !value);
-                setError("");
-              }}
-            >
-              {isLoginForm ? (
-                <>
-                  New User?{" "}
-                  <span className="text-purple-500 hover:text-purple-400 transition duration-300">
-                    Signup Here
-                  </span>
-                </>
-              ) : (
-                <>
-                  Existing User?{" "}
-                  <span className="text-purple-500 hover:text-purple-400 transition duration-300">
-                    Login Here
-                  </span>
-                </>
-              )}
-            </p>
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text font-medium">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    className="input input-bordered w-full max-w-xs"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </label>
+              </>
+            )}
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text font-medium">Email ID</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                className="input input-bordered w-full max-w-xs"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text font-medium">Password</span>
+              </div>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <span
+                  className="absolute right-3 top-3 cursor-pointer text-gray-500 hover:text-gray-700"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  {showPassword ? (
+                    <AiOutlineEyeInvisible size={20} />
+                  ) : (
+                    <AiOutlineEye size={20} />
+                  )}
+                </span>
+              </div>
+            </label>
           </div>
+          <p className="text-red-500">{error}</p>
+          <div className="card-actions justify-center m-2">
+            <button
+              className="btn btn-primary uppercase"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+            >
+              {isLoginForm ? "Login" : "Sign Up"}
+            </button>
+          </div>
+
+          <p
+            className="m-auto cursor-pointer py-2 font-semibold"
+            onClick={() => {
+              setIsLoginForm((value) => !value);
+              setError("");
+            }}
+          >
+            {isLoginForm ? (
+              <>
+                New User?{" "}
+                <span className="text-purple-500 hover:text-purple-400 transition duration-300">
+                  Signup Here
+                </span>
+              </>
+            ) : (
+              <>
+                Existing User?{" "}
+                <span className="text-purple-500 hover:text-purple-400 transition duration-300">
+                  Login Here
+                </span>
+              </>
+            )}
+          </p>
         </div>
       </div>
-      <Footer />
-    </>
+    </div>
   );
 };
 export default Login;
